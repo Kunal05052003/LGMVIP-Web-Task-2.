c7d5ecb989f9e7fd96c5b70c3cdeb0591095a228
kunal <kunal.jadhav_comp21@pccoer.in>
2023-07-24 20:38:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import React from "react";
+import Users from './Components/Users';
+
+
+function App(){
+  return(
+    <div className="App">
+      <h1>Users Information</h1>
+      <Users/>
+      
+    </div>
+    
+  )
+}
+
+export default App;
+
